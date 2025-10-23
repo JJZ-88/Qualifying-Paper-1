e9a4639aef38274d53b988b8c82c2ad9ceea0b4d
JustinZhang <justin.zhang@dragonagile.com>
2025-10-22 18:39:34 -0700

    for tracking

--- src/estimating_epidemics.R
+++ src/estimating_epidemics.R
@@ -1,13 +1,14 @@
 library(rtestim)
 library(ggplot2)
 library(signal)
+library("gridExtra")
 
 
 ##Simulated epidemic scenarios
 get_R_true <- function(scen_num, n_days){
   R_true <- c() #true instantaneous reproduction numbers
   if(scen_num == 1){ #piecewise constant
-    R_true <- c(rep(2.5, 70), rep(0.5, 160), rep(2.5, n_days - 230))
+    R_true <- c(rep(2, 100), rep(3, 100), rep(0.5, n_days - 200))
   }
   else if(scen_num == 2){ #sinusoidal
     R_true <- 1.3 + 1.2 * sin(pi * seq(1, n_days, by = 1) / 60)
@@ -16,18 +17,22 @@ get_R_true <- function(scen_num, n_days){
     R_true <- c(exp(0.03 * seq(1, 40, by = 1)), exp(-0.015 * seq(1, 150, by = 1) + 1.2), 
                 exp(0.02 * seq(1, n_days - 190, by = 1) - 1.05))
   }
-  
+  else if(scen_num == 4){
+    #R_true <- c(1.5 + 0.01 * seq(1, 150, 1), 3 - 0.01 * seq(1, 150, 1))
+    R_true <- rep(1.2, 300) + rep(c(rep(0, 55), rep(0.5, 5)), 5)
+  }
   return(R_true)#list('R_true' = R_true, 'scen' = scen_num))
 }
 
-#get 'probability mass' of gamma distribution (serial time dist.)
+
+##'Probability Mass' of gamma distribution (serial time dist.)
 get_dist_vals <- function(dist_param, n_days){
   dist <- pgamma(seq(1, n_days, by = 1), dist_param[1], rate = dist_param[2]) - 
     pgamma(seq(0, n_days - 1, by = 1), dist_param[1], rate = dist_param[2])
   return(rev(dist)) #return dist in reverse time, so that w_1 is in the last index
 }
 
-#get the true R_t, incidence, infectiousness numbers
+##Get true R_t, incidence, infectiousness numbers
 setup <- function(scen_num, n_days, dist_param, init_inc, offset){
   R_true <- get_R_true(scen_num, n_days) #get R_t and w_t based on preset parameters
   dist <- get_dist_vals(dist_param, n_days)
@@ -40,9 +45,6 @@ setup <- function(scen_num, n_days, dist_param, init_inc, offset){
     for(i in 2:n_days){ #iterate over days
       #Lam_t = sum_{j=1}^{i-1} I_{i-j} * w_j 
       lam_true[i] <- sum(I_true[1:i - 1] * dist[(n_days - i + 2):n_days])
-      #if(i < 100){
-      #  print(dist[(n_days - i + 2):n_days])
-      #}
       
       #I_t ~ Poisson(lam_t * R_t)
       I_true[i] <- rpois(1, lam_true[i] * R_true[i])
@@ -51,47 +53,274 @@ setup <- function(scen_num, n_days, dist_param, init_inc, offset){
       small_epi = F
     }
   }
-
   return(list('R_true' = R_true[(offset+1):n_days], 'I_true' = I_true[(offset+1):n_days], 
               'lam_true' = lam_true[(offset+1):n_days]))
 }
 
+##Run rtestim package to get optimal (tuned) fit
+run_rtestim <- function(incidence, dist_param){
+  rtestim_lambda <- cv_estimate_rt(incidence, nfold = 5, korder = 3, dist_gamma = c(dist_param[1], 1 / dist_param[2]))
+  rtestim_fit <- fitted(rtestim_lambda, 'lambda.min')
+  rtestim_ci <- confband(rtestim_lambda, lambda = 'lambda.min')
+  return(list('lambda' = rtestim_lambda$lambda.min, 'fit' = rtestim_fit, 'ci' = rtestim_ci))
+}
 
+##Compute r_t from R_t from Wallinga Lipsitch
+get_rt <- function(R, dist_param){
+  return((R^(1 / dist_param[1]) - 1) * dist_param[2])
+}
+
+simulate_epidemic <- function(n_days, dist_param, init_inc, offset, scen_num, save_plot, file_name){
+  ##Derive true values and model estimates
+  epi_vals <- setup(scen_num, n_days, dist_param, init_inc, offset) #get true values
+  r_lam <- diff(log(epi_vals$lam_true)) #r_t | lam_t as pairwise difference of log(lam_t)
+  tau <- round(dist_param[1] / dist_param[2] / 2)
+  #get the true r_t from true R_t based on same model assumptions
+  r_true <- get_rt(epi_vals$R_true, dist_param)
+  #esimate Rt using rtestim package
+  R_est <- run_rtestim(epi_vals$I_true, dist_param)
+  #smoothed incidence using SG filter (window 50, cubic fit)
+  I_emp <- pmax(sgolayfilt(epi_vals$I_true, p = 3, n = 31), rep(0.00001, n_days - offset)) 
+  r_emp <- sgolayfilt(diff(log(I_emp)), p = 3, n = 91) #r_t | S_t from smoothed incidence
+  r_Rt <- get_rt(R_est$fit, dist_param)
+  r_upperCI <- get_rt(R_est$ci$`97.5%`, dist_param)
+  r_lowerCI <- get_rt(R_est$ci$`2.5%`, dist_param)
+  
+  
+  ##Plot 1: estimating reproduction numbers
+  g1 <- ggplot(data.frame(x = seq(offset + 1, n_days, 1), y = epi_vals$R_true, z = R_est$fit)) +
+    geom_line(aes(x = x, y = y, colour = 'R_true')) +
+    geom_line(aes(x = x, y = z, colour = 'R_estimate')) +
+    geom_line(aes(x = x, y = R_est$ci$`2.5%`, colour = 'R_estimate'), linetype = 'dotted') +
+    geom_line(aes(x = x, y = R_est$ci$`97.5%`, colour = 'R_estimate'), linetype = 'dotted') +
+    scale_color_manual(values = c('R_true' = 'black', 'R_estimate' = 'blue')) + 
+    theme(legend.position = "bottom") +
+    labs(title = 'Estimating Instantaneous \n Reproduction Number', x = 'Days', y = 'Reproduction Number', 
+         color = '', tag = 'a)')
+  
+  ##Plot 2: estimating growth rates 3 ways
+  g2 <- ggplot() + 
+    geom_line(aes(x = seq(offset + 1, n_days, 1), y = r_true, colour = 'r_true')) +
+    geom_line(aes(x = seq(offset + tau + 1, n_days, 1), y = r_emp[1:(length(r_emp) - tau + 1)], colour = 'r_t | S_t')) +
+    geom_line(aes(x = seq(offset + 1, n_days - tau, 1), y = r_lam[tau:length(r_lam)], colour = 'r_t | lam_t')) +
+    geom_line(aes(x = seq(offset + 1, n_days, 1), y = r_Rt, colour = 'r_t | R_t')) +
+    geom_line(aes(x = seq(offset + 1, n_days, 1), y = r_upperCI, colour = 'r_t | R_t'), linetype = 'dotted') +
+    geom_line(aes(x = seq(offset + 1, n_days, 1), y = r_lowerCI, colour = 'r_t | R_t'), linetype = 'dotted') +
+    scale_color_manual(values = c('r_true' = 'black', 'r_t | S_t' = 'blue', 'r_t | lam_t' = 'red', 'r_t | R_t' = 'green')) +
+    theme(legend.position = "bottom") +
+    labs(title = 'Estimating Instantaneous \n Growth Rate',x = 'Days', y = 'Growth Rate', 
+         color = '', tag = 'b)')
+  
+  ##Plot 3: incidence
+  #Here, we use I_t coming from a Poisson distribution with rate (R_t lam_t)
+  g3 <- ggplot(data.frame(x = seq(offset + 1, n_days, 1), y = epi_vals$I_true, z = epi_vals$lam_true * R_est$fit)) + 
+    geom_point(aes(x = x, y = y, colour = 'I_true'), size = 0.2) +
+    geom_line(aes(x = x, y = z, colour = 'I_t | R_t')) +
+    scale_color_manual(values = c('I_true' = 'black', 'I_t | R_t' = 'blue')) + 
+    theme(legend.position = "bottom") +
+    labs(x = 'Days', y = 'Incidence of Infection', title = 'Estimating Incidence \n of Infection', 
+         color = '', tag = 'c)')
+  
+  ##Plot 4: smoothed incidence curves
+  g4 <- ggplot(data.frame(x = seq(offset + 1, n_days - 2 * tau, 1), y = epi_vals$I_true[1:(n_days - offset - 2 * tau)], 
+                    z = I_emp[1:(n_days - offset - 2 * tau)], w = epi_vals$lam_true[(2 * tau + 1):(n_days - offset)])) + 
+    geom_point(aes(x = x, y = y, colour = 'I_t'), size = 0.2) +
+    geom_line(aes(x = x, y = z, colour = 'S_t')) +
+    geom_line(aes(x = x, y = w, colour = 'lam_{t + 2tau}')) +
+    scale_color_manual(values = c('I_t' = 'black', 'S_t' = 'blue', 'lam_{t + 2tau}' = 'red')) + 
+    theme(legend.position = "bottom") +
+    labs(x = 'Days', y = 'Incidence of Infection', title = 'Smoothing Incidence \n of Infection', 
+         color = '', tag = 'd)')
+  
+  print(g1)
+  print(g2)
+  print(g3)
+  print(g4)
+  
+  if(save_plot == 1){
+    to_plot <- grid.arrange(g1, g2, g3, g4, ncol = 2, nrow = 2)
+    ggsave(file_name, to_plot)
+  }
+  if(save_plot == 2){
+    to_plot <- grid.arrange(g1, g2, g3, g4, ncol = 2, nrow = 2)
+    ggsave(file_name, to_plot)
+  }
+  
+  return(list('R_true' = epi_vals$R_true, 'I_true' = epi_vals$I_true, 'r_est' = r_Rt,
+              'lam_true' = epi_vals$lam_true,'r_true' = r_true, 'R_est' = R_est$fit))
+}
+test <- simulate_epidemic(n_days, dist_param, init_inc, offset, 4, 0, '')
+
+kl <- function(R_true, R_est, lambda_true){
+  return(sum(lambda_true * (R_true * log(R_true / R_est) + R_est - R_true)))
+}
+
+mse <- function(r_true, r_est){
+  return(sum((r_true - r_est)^2))
+}
+
+
+##Initialize parameters
 n_days <- 300
 #dist_param <- c((1 / 0.65)^2, (1 / 0.65)^2 / 6.5)
 dist_param <- c(2.7066, 2.7066 / 15.3)
 init_inc <- 10
 offset <- 20
+scen_num <- 2
 
-epi_vals <- setup(2, n_days, dist_param, init_inc, offset) #get true values
-epi_vals
-r_lam <- diff(log(epi_vals$lam_true)) #r_t|lam_t as pairwise difference of log(lam_t)
-tau <- 8
-#get the true r_t from true R_t based on same model assumptions
-r_true <- (epi_vals$R_true^(1 / dist_param[1]) - 1) * dist_param[2]
+setwd("/Users/JZ/Desktop/Qualifying-Paper-1/report/fig")
+epi1 <- simulate_epidemic(n_days, dist_param, init_inc, offset, scen_num, 1, 'epi_paper.pdf')
 
-rtestim_lambda <- cv_estimate_rt(epi_vals$I_true + 1, nfold = 5, korder = 3)
-rtestim_fit <- fitted(rtestim_lambda, 'lambda.min')
-#rtestim_fit <- estimate_rt(epi_vals$I_true + 0.001, lambda = 0)
-
-plot(seq(1, 280, 1), epi_vals$R_true, type = 'l')
-lines(rtestim_fit)
-
-I_emp <- sgolayfilt(epi_vals$I_true, p = 3, n = 51)
-r_emp <- sgolayfilt(diff(log(I_emp)), p = 3, n = 91)
-
-#plot 2: growth rates
-plot(seq(1, 280, 1), r_true, type = 'l')
-lines(seq(tau + 1, 280, 1), r_emp[1:(length(r_emp) - tau + 1)], col = 'blue')
-lines(seq(1, 280 - tau, 1), r_lam[tau:length(r_lam)], col = 'red')
-lines(seq(1, 280, 1), 
-      (rtestim_fit^(1 / dist_param[1]) - 1) * dist_param[2])
+kl(epi1$R_true, epi1$R_est, epi1$lam_true)
+mse(epi1$r_true, epi1$r_est)
 
 
+##Estimates with misspecified serial interval distribution
+dist_param_misspec <- c(2.7066, 2.7066 / 15.3 * 3) #misspecify gamma mean as 1/3 of true mean
+dist_misspec <- get_dist_vals(dist_param_misspec, n_days)
+
+Rt_misspec <- run_rtestim(epi1$I_true, dist_param_misspec)
+
+kl(epi1$R_true, Rt_misspec$fit, epi1$lam_true)
+mse(epi1$r_true, get_rt(Rt_misspec$fit, dist_param_misspec))
+
+##Plot 5: estimating reproduction numbers under misspecification of serial interval distribution
+irr_misspec <- ggplot(data.frame(x = seq(offset + 1, n_days, 1), y = epi1$R_true, z = epi1$R_est, w = Rt_misspec$fit)) +
+  geom_line(aes(x = x, y = y, colour = 'R_true')) +
+  geom_line(aes(x = x, y = z, colour = 'R_est')) +
+  geom_line(aes(x = x, y = w, colour = 'R_est (misspecified)')) +
+  geom_line(aes(x = x, y = Rt_misspec$ci$`2.5%`, colour = 'R_est (misspecified)'), linetype = 'dotted') +
+  geom_line(aes(x = x, y = Rt_misspec$ci$`97.5%`, colour = 'R_est (misspecified)'), linetype = 'dotted') +
+  scale_color_manual(values = c('R_true' = 'black', 'R_est' = 'blue', 'R_est (misspecified)' = 'red')) + 
+  labs(title = 'Estimating Instantaneous Reproduction Number under \n Misspecied Serial Interval Distribution', 
+       x = 'Days', y = 'Reproduction Number', color = 'Reproduction Number')
+
+##Plot 6: estimating growth rates under misspecification of serial interval distribution
+gr_misspec <- ggplot(data.frame(x = seq(offset + 1, n_days, 1), y = epi1$r_true, z = get_rt(Rt_misspec$fit, dist_param_misspec), 
+                  w = get_rt(Rt_misspec$ci$`97.5%`, dist_param_misspec), v = get_rt(Rt_misspec$ci$`2.5%`, dist_param_misspec))) + 
+  geom_line(aes(x = x, y = y, colour = 'r_true')) +
+  geom_line(aes(x = x, y = z, colour = 'r_t | R_t')) +
+  geom_line(aes(x = x, y = w, colour = 'r_t | R_t'), linetype = 'dotted') +
+  geom_line(aes(x = x, y = v, colour = 'r_t | R_t'), linetype = 'dotted') +
+  scale_color_manual(values = c('r_true' = 'black', 'r_t | R_t' = 'blue')) +
+  labs(title = 'Estimating Instantaneous Growth Rate',x = 'Days', y = 'Growth Rate', color = 'Growth Rate')
+
+to_plot_misspec <- grid.arrange(irr_misspec, gr_misspec, ncol = 1, nrow = 2)
+ggsave('epi_misspec.pdf', to_plot_misspec)
 
 
+test_epi <- simulate_epidemic(n_days, dist_param, init_inc, offset, 1, 0, '')
+kl(test_epi$R_true, test_epi$R_est, test_epi$lam_true)
+
+##Penalized r_t - mini proposal
+
+#make nth differences matrix
+make_div_diff <- function(n, it){
+  d <- cbind(rep(0, n - 1), diag(rep(1, n - 1))) + cbind(diag(rep(-1, n - 1)), rep(0, n - 1)) #first differences
+  if(it > 1){ 
+    for(i in 1:(it - 1)){ #for nth differences greater than 2, make 1st differences matrix of 1 dimension lower and multiply to d
+      d0 <- cbind(rep(0, n - 1 - i), diag(rep(1, n - 1 - i))) + cbind(diag(rep(-1, n - 1 - i)), rep(0, n - 1 - i))
+      d <- d0 %*% d
+    }
+  }
+  return(d)
+}
+
+#objective function to minimize
+objective_fn <- function(r, inc, lambda_1, lambda_2, d, d2){
+  n <- length(inc)
+  loss <- sum(-inc[2:n]*(log(inc[1:(n - 1)]) + r[2:n]) + inc[1:(n - 1)] * exp(r[2:n])) #poisson loss
+  pen <- lambda_1 * sum(exp((d %*% r)^2))  #penalize first differences
+  pen_2 <- lambda_2 * sum(exp((d2 %*% r)^2)) #penalize second differences
+  #pen <- lambda_1 * sum((exp(r[2:n]) - exp(r[1:(n-1)]))^2)
+  #pen <- lambda * sum((d %*% r)^2)
+  return(loss + pen + pen_2)
+}
+
+#use optim function to minimize objective
+#NOTE EACH CALL TO OBJECTIVE NEEDS TO CHANGE FUNCTION objective_fn ACCORDINGLY
+obj_fs = optim(rep(0, 280), fn = objective_fn, inc = e, d = make_div_diff(280, 1), d2 = make_div_diff(280, 2), lambda_1 = 200, lambda_2 = 500, method = 'BFGS')
+
+plotr_fsdiff <- ggplot(data.frame(x = seq(tau + 1, n_days - offset, 1), y = obj_fs$par[1:(n_days - offset - tau)],z = epi1$r_true[(tau + 1):(n_days - offset)])) +
+  geom_line(aes(x = x, y = y, colour = 'r_est')) +
+  geom_line(aes(x = x, y = z, colour = 'r_true')) +
+  scale_color_manual(values = c('r_true' = 'black', 'r_est' = 'blue')) + 
+  theme(legend.position = "bottom") +
+  labs(title = 'Smoothed Estimator of Growth Rate \n
+       with First and Second Differences',x = 'Days', y = 'Growth Rate', color = '', tag = 'c')
+
+plot(plotr_fsdiff)
+obj_f = optim(rep(0, 280), fn = objective_fn, inc = e, d = make_div_diff(280, 1), d2 = make_div_diff(280, 2), lambda_1 = 200, lambda_2 = 500, method = 'BFGS')
+
+plotr_fdiff <- ggplot(data.frame(x = seq(tau + 1, n_days - offset, 1), y = obj_f$par[1:(n_days - offset - tau)],z = epi1$r_true[(tau + 1):(n_days - offset)])) +
+  geom_line(aes(x = x, y = y, colour = 'r_est')) +
+  geom_line(aes(x = x, y = z, colour = 'r_true')) +
+  scale_color_manual(values = c('r_true' = 'black', 'r_est' = 'blue')) + 
+  theme(legend.position = "bottom") +
+  labs(title = 'Smoothed Estimator of Growth Rate \n
+       with First Differences',x = 'Days', y = 'Growth Rate', color = '', tag = 'b')
+
+obj_mle = optim(rep(0, 280), fn = objective_fn, inc = e, d = make_div_diff(280, 1), d2 = make_div_diff(280, 2), lambda_1 = 200, lambda_2 = 500, method = 'BFGS')
+
+plotr_mle <- ggplot(data.frame(x = seq(tau + 1, n_days - offset, 1), y = obj_mle$par[1:(n_days - offset - tau)],z = epi1$r_true[(tau + 1):(n_days - offset)])) +
+  geom_line(aes(x = x, y = y, colour = 'r_est')) +
+  geom_line(aes(x = x, y = z, colour = 'r_true')) +
+  scale_color_manual(values = c('r_true' = 'black', 'r_est' = 'blue')) + 
+  theme(legend.position = "bottom") +
+  labs(title = 'Smoothed Estimator of Growth Rate \n
+       with MLE',x = 'Days', y = 'Growth Rate', color = '', tag = 'a')
   
-  
-  
-  
-  
+to_plot_smoothrt <- grid.arrange(plotr_mle, plotr_fdiff, plotr_fsdiff, nrow = 2)
+ggsave('estimate_growthrate.pdf', to_plot_smoothrt)
+
+#used for basic tuning of lambda parameters
+lambda_test <- seq(400, 800, by = 20)
+lambda_mse <- seq(400, 800, by = 20)
+for(l in 1:length(lambda_test)){
+  obj = optim(rep(0, 280), fn = f, inc = e, d = make_div_diff(280, 1), d2 = make_div_diff(280, 2), lambda = lambda_test[l], method = 'BFGS')
+  lambda_mse[l] <- mse(obj$par[1:(n_days - offset - tau)], epi1$r_true[(tau + 1):(n_days - offset)])
+}
+data.frame(x = lambda_test, mse = lambda_mse)
+
+
+
+#TESTING
+
+#compare lambda and s
+a = rev(get_dist_vals(dist_param, 300))[1:51]
+plot(seq(1, 51, 1), a/sum(a), ylim = c(-0.05, 0.1))
+points(seq(1, 40, 1), rev(sgolay(n = 51, p = 3)[26,1:40]), col = 'red')
+
+s = sgolayfilt(epi1$I_true, n = 51, p = 3)
+t(epi1$I_true[100:150]) %*% sgolay(n = 51, p = 3)
+
+plot(diff(epi1$lam_true[16:280]))
+points(diff(s[1:265]), col = 'red')
+
+test <- diff(log(epi_vals$I_true + 1))
+a <- epi_vals$I_true
+#test <- ifelse(a[1:279] == 0, 1, ifelse(a[2:280] == 0, -1, log(a[2:280]) - log(a[1:279]) )) 
+#test
+plot(seq(tau + 1, n_days - offset, 1), sgolayfilt(test, p = 3, n = 51)[1:(n_days - offset - tau)], type = 'l')
+lines(seq(1, n_days - offset - 1, 1), epi1$r_true[2:280])
+
+#james stein
+e = epi1$I_true + 1
+si = sum(e^2)
+n = length(e)
+vari = var(e)
+tau = 8
+#jsr = (1 - (ni - 2) * vari / ni / si) * e
+jsr = (1 - vari / si) * e
+
+s = sgolayfilt(log(jsr[2:280] / e[1:279]), p = 3, n = 51)[1:(n_days - offset - tau)]
+s1 =  sgolayfilt(diff(log(e)), p = 3, n = 51)[1:(n_days - offset - tau)]
+plot(seq(tau + 1, n_days - offset, 1), s, type = 'l')
+lines(seq(1, n_days - offset - 1, 1), epi1$r_true[2:280])
+lines(seq(tau + 1, n_days - offset, 1), s1, type = 'l', col = 'red')
+mse(epi1$r_true[(tau+1):280],s)
+
+
+
+
+
